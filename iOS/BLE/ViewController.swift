bb940b0231bdd3746094b2637309d331d7bf48d8
Leonardo <leo@leocardz.com>
2017-11-22 16:52:31 +0100

    ViewController

--- iOS/BLE/ViewController.swift
+++ iOS/BLE/ViewController.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2017 leocardz.com. All rights reserved.
 //
 
+import UIKit
 
+class ViewController: UIViewController {
+
+    // MARK: - IBOutlet
+    @IBOutlet var label: UILabel!
+
+    // MARK: - Lifecyle
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+
+    // MARK: - Properties
+    var manager: BluetoothManager?
+
+    override func viewDidAppear(_ animated: Bool) {
+
+        super.viewDidAppear(animated)
+
+        self.manager = BluetoothManager(delegate: self)
+
+    }
+
+}
+
+// MARK: - BlueEar
+extension ViewController: BlueEar {
+
+    func didStartConfiguration() { self.label.text = "Start configuration 🎛" }
+
+    func didStartAdvertising() { self.label.text = "Start advertising 📻" }
+
+    func didSendData() { self.label.text = "Did send data ⬆️" }
+
+    func didReceiveData() { self.label.text = "Did received data ⬇️" }
+
+}
